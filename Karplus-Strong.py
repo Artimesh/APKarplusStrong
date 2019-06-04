fe91c940a68d92218c035b7e8de0107e54e1e02b
Artimesh <nkris17@student.aau.dk>
2019-06-04 12:28:33 +0200

    Added some 'instructions' on how to play and what plays.

--- Karplus-Strong.py
+++ Karplus-Strong.py
@@ -36,35 +36,38 @@ noiseBurst = np.r_[np.random.randn(200),np.zeros(nData-200)]
 #The filter coefficient for the comb filter.
 combParameter = 0.99
 
+print('Instructions: ')
+print('Play notes by pressing the following buttons: A, W, S, E, D, F, T, G, Y, H, U, J')
+print('The keys correspond to the following chords: ')
+print('C3, C3#, D3, D3#, E3, F3, F3#, G3, G3#, A3, A3#, B3')
+
 while True:
-    #x = input("What note should be played?: ")
-    print('Input note to play: ')
     x = msvcrt.getch().lower().decode()
     #Following is a messy way of checking what the user input is
     #and then sets the pitch based on the input
-    if x == "c":
+    if x == "a":    #C3
         pitch = 130.81
-    elif x == "csharp":
+    elif x == "w":  #C3Sharp
         pitch = 138.59
-    elif x == "d":
+    elif x == "s":  #D3
         pitch = 146.83
-    elif x == "dsharp":
+    elif x == "e":  #D3Sharp
         pitch = 155.56
-    elif x == "e":
+    elif x == "d":  #E3
         pitch = 164.81
-    elif x == "f":
+    elif x == "f":  #F3
         pitch = 174.61
-    elif x == "fsharp":
+    elif x == "t":  #F3Sharp
         pitch = 185
-    elif x == "g":
+    elif x == "g":  #G3
         pitch = 196
-    elif x == "gsharp":
+    elif x == "y":  #G3Sharp
         pitch = 207.65
-    elif x == "a":
+    elif x == "h":  #A3
         pitch = 220
-    elif x == "asharp":
+    elif x == "u":  #A3Sharp
         pitch = 233.08
-    elif x == "b":
+    elif x == "j":  #B3
         pitch = 246.94
     else:
         pitch = 0
